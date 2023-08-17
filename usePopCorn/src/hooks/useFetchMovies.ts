fc05b5d6e5ebb7884e1ae3f0bc361c14e9352c4e
juanan fernandez <juanan.fernandez@gmail.com>
2023-08-17 20:51:51 +0200

    add fetchMovies custom hook

--- usePopCorn/src/hooks/useFetchMovies.ts
+++ usePopCorn/src/hooks/useFetchMovies.ts
@@ -1,5 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Movie } from '../interfaces/movies';
 
-export function useFetchMovies(): {} {
+interface Error {
+	errMessage: string;
+}
+
+interface fetchMoviesResponse {
+	movies: Movie[];
+	terror: Error | undefined; // if there is an error, it will be stored in this variable. Otherwise undefined (optional)
+	isLoading: boolean;
+}
+
+export function useFetchMovies(url: string): fetchMoviesResponse {
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [terror, setTerror] = useState();
+
+	useEffect(() => {
+		async function fetchMovies(): Promise<void> {
+			try {
+				const response = await fetch(url);
+				if (!response.ok) {
+					throw new Error('Error al obtener los datos');
+				}
+			} catch (err) {
+				console.log('ERROR', err);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+
+		fetchMovies();
+	}, [url]);
+
+	return { movies, terror, isLoading };
 }
---
+++ usePopCorn/src/interfaces/movies.d.ts
@@ -0,0 +1,47 @@
+interface Movie {}
+
+export interface SearchMovie {
+	Response: string;
+	Search: Movie[];
+	totalResults: string;
+}
+
+export interface Movie {
+	Poster: string;
+	Title: string;
+	Year: string;
+	imdbID: string;
+}
+
+export interface MovieDetails {
+	Actors: string;
+	Awards: string;
+	BoxOffice: string;
+	Country: string;
+	DVD: string;
+	Director: string;
+	Genre: string;
+	Language: string;
+	Metascore: string;
+	Plot: string;
+	Poster: string;
+	Production: string;
+	Rated: string;
+	Ratings: Rating[];
+	Released: string;
+	Response: string;
+	Runtime: string;
+	Title: string;
+	Type: string;
+	Website: string;
+	Writer: string;
+	Year: string;
+	imdbID: string;
+	imdbRating: string;
+	imdbVotes: string;
+}
+
+export interface Rating {
+	Source: string;
+	Value: string;
+}
