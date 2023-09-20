5c96ba72c150013f313c3c505007f4c09670445e
juanan fernandez <juanan.fernandez@gmail.com>
2023-09-20 20:53:32 +0200

    fix error in form SplitBill -> the values were not reset when selecting a different friend

--- eat-n-split/src/components/Friends/Friends.tsx
+++ eat-n-split/src/components/Friends/Friends.tsx
@@ -1,39 +1,39 @@
-import style from './Friends.module.css'
-import { useState } from 'react'
-import { initialFriends } from '../../dummy/data'
-import { FriendsList } from './FriendsList'
-import { FriendWithId } from '../../types/friends'
-import { SplitBill } from '../SplitBill/SplitBill'
+import style from './Friends.module.css';
+import { useState } from 'react';
+import { initialFriends } from '../../dummy/data';
+import { FriendsList } from './FriendsList';
+import { FriendWithId } from '../../types/friends';
+import { SplitBill } from '../SplitBill/SplitBill';
 
 export function Friends(): React.ReactNode {
-	const [friendsData, setFriendsData] = useState(initialFriends)
-	const [activeFriend, setActiveFriend] = useState<FriendWithId | null>(null)
-	const [activeFriendId, setActiveFriendId] = useState('')
+	const [friendsData, setFriendsData] = useState(initialFriends);
+	const [activeFriend, setActiveFriend] = useState<FriendWithId | null>(null);
+	const [activeFriendId, setActiveFriendId] = useState('');
 
 	const clearActiveFriend = (): void => {
-		setActiveFriend(null)
-		setActiveFriendId('')
-	}
+		setActiveFriend(null);
+		setActiveFriendId('');
+	};
 	const activeFriendHandler = (active: FriendWithId): void => {
 		if (activeFriend && activeFriend.id === active.id) {
-			clearActiveFriend()
-			return
+			clearActiveFriend();
+			return;
 		}
-		setActiveFriend(active)
-		setActiveFriendId(active.id)
-	}
+		setActiveFriend(active);
+		setActiveFriendId(active.id);
+	};
 
 	const addFriendToListHandler = (newFriend: FriendWithId): void => {
 		//setFriendsData([...friendsData, newFriend])
-		setFriendsData(currentFriends => [...currentFriends, newFriend])
-	}
+		setFriendsData(currentFriends => [...currentFriends, newFriend]);
+	};
 
 	const updateFriendHandler = (myFriend: FriendWithId): void => {
 		const updatedFriendsList = friendsData.map(f => {
-			return f.id === myFriend.id ? myFriend : f
-		})
-		setFriendsData(updatedFriendsList)
-	}
+			return f.id === myFriend.id ? myFriend : f;
+		});
+		setFriendsData(updatedFriendsList);
+	};
 
 	return (
 		<div className={style['friends-app']}>
@@ -45,9 +45,14 @@ export function Friends(): React.ReactNode {
 			/>
 			{activeFriend && (
 				<div>
-					<SplitBill friend={activeFriend} onUpdateFriend={updateFriendHandler} clearActiveFriend={clearActiveFriend} />
+					<SplitBill
+						friend={activeFriend}
+						onUpdateFriend={updateFriendHandler}
+						clearActiveFriend={clearActiveFriend}
+						key={activeFriend.id}
+					/>
 				</div>
 			)}
 		</div>
-	)
+	);
 }
